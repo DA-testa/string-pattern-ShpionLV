275a693d483c2d74577fcefeeff722f094ab942c
ShpionLV <60115605+ShpionLV@users.noreply.github.com>
2023-04-14 21:31:26 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -12,10 +12,8 @@ def read_input(filename):
 
             pattern = contents[0].strip()
             text = contents[1].strip()
-    
-    return pattern, text
-
-    def read_input():
+ 
+    else:
         pattern = input().rstrip()
         text = input().rstrip()
     
@@ -35,26 +33,25 @@ def read_input():
         raise ValueError("Invalid input type")
 
 def print_occurrences(output):
-   
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
     prime = 101
     base = 256
 
-    pattern = 0
-    text = 0
+    p_hash = 0
+    t_hash = 0
     for i in range(len(pattern)):
-        pattern = (pattern * base + ord(pattern[i])) % prime
-        text = (text * base + ord(text[i])) % prime
+        p_hash = (p_hash * base + ord(pattern[i])) % prime
+        t_hash = (t_hash * base + ord(text[i])) % prime
 
     occurrences = []
     for i in range(len(text) - len(pattern) + 1):
-        if pattern == text:
+        if p_hash == t_hash:
             if pattern == text[i:i+len(pattern)]:
                 occurrences.append(i)
         if i < len(text) - len(pattern):
-            text = ((text - ord(text[i]) * (base**(len(pattern)-1))) * base + ord(text[i+len(pattern)])) % prime
+            t_hash = ((t_hash - ord(text[i]) * (base**(len(pattern)-1))) * base + ord(text[i+len(pattern)])) % prime
 
     return occurrences
 
